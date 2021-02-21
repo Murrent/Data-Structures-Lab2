914cadeee3bc807bb6e39ba5d026429f0cb86b6d
Tempron <wickgrenm@gmail.com>
2021-02-21 18:52:37 +0100

    added list

---
+++ cmake-build-debug/CMakeFiles/Lab2SM_Clion.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab2SM_Clion.dir/src/list/list.c.obj"
+  "CMakeFiles/Lab2SM_Clion.dir/src/main.c.obj"
+  "Lab2SM_Clion.exe"
+  "Lab2SM_Clion.exe.manifest"
+  "Lab2SM_Clion.pdb"
+  "libLab2SM_Clion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab2SM_Clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
