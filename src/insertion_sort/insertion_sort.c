3b90bcdce9bd19683a815774f3dae39bd087e3ba
Tempron <wickgrenm@gmail.com>
2021-02-22 11:58:47 +0100

    insertion_sort

---
+++ src/insertion_sort/insertion_sort.c
@@ -0,0 +1,5 @@
+//
+// Created by Martin on 2021-02-22.
+//
+
+#include "insertion_sort.h"
---
+++ src/insertion_sort/insertion_sort.h
@@ -0,0 +1,8 @@
+//
+// Created by Martin on 2021-02-22.
+//
+
+#ifndef LAB2SM_CLION_INSERTION_SORT_H
+#define LAB2SM_CLION_INSERTION_SORT_H
+
+#endif //LAB2SM_CLION_INSERTION_SORT_H
