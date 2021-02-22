f81e820d5397a83917cce38de04f80308ab8b811
Tempron <wickgrenm@gmail.com>
2021-02-22 10:48:11 +0100

    load file added

---
+++ src/load_files/test_load_file.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "load_file.h"
+
+int main( int argc, char* argv[] )
+{
+	const char* fName = argv[1];
+	
+	int* a = load_file(fName);
+
+	int size = a[0];
+	for ( int i = 1 ; i <= size ; i++ ) // note this loops from 1 <= i <= size
+	{
+		printf("%d\n", a[i]);
+	}
+    return 0;
+}
