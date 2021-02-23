3394c4cf510f1739507808daf76d975f6a23ad27
Samson <xsates25@hotmail.com>
2021-02-23 23:23:29 +0100

    ar

--- src/array/array.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Namn on 2021-02-22.
-//
-
-#include "array.h"
--- src/array/array.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Namn on 2021-02-22.
-//
-
-#ifndef LAB2SM_CLION_ARRAY_H
-#define LAB2SM_CLION_ARRAY_H
-
-#endif //LAB2SM_CLION_ARRAY_H
